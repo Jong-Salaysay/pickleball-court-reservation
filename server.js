1993a81228dafbc0a3e67148e43218689943826a
Jong-Salaysay <155979500+Jong-Salaysay@users.noreply.github.com>
2026-07-06 09:19:56 +0800

    Registration with hashed passwords

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
 const express = require("express");
+const db = require("./db/db");
 const app = express();
+const bcrypt = require("bcryptjs");
 
 app.use(express.static ("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/test", async (req, res) => {
+    const [rows] = await db.query("SELECT 1 + 1 AS result");
+    res.json(rows);
+});
 
 app.listen(3000, () =>{
     console.log("Server is running at http://localhost:3000");
+});
+
+app.post("/api/auth/register", async (req, res) => {
+    console.log(req.body);
+    const { first_name, last_name, email, contact_number, password, confirm_password } = req.body;
+
+    if (!first_name || !last_name || !email || !contact_number || !password) {
+    return res.status(400).send({ error: "All fields are required" });
+    }
+    if (password !== confirm_password) {
+        return res.status(400).send({ error: "Passwords do not match" });
+    }
+
+    try {
+        const password_hash = await bcrypt.hash(password, 10);
+        
+        await db.query(
+            "INSERT INTO users (email, password_hash, first_name, last_name, contact_number) VALUES (?, ?, ?, ?, ?)",
+            [email, password_hash, first_name, last_name, contact_number]
+        );
+
+        res.redirect("/login.html");
+    } catch (error) {
+        if (error.code === "ER_DUP_ENTRY") {
+            return res.status(409).send("Email already exists");
+        }
+        console.error(error);
+        res.status(500).send("Something went wrong. Please try again.");
+    }
+
 });
